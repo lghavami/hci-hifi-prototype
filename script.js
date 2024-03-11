1aba4f8bae0ef3437e036f627a1cc5ae84bc0803
lghavami <leylighavami@gmail.com>
2024-03-11 20:15:30 +0100

    more detail to events and added languages page

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-// upon page load
-async function pageLoadHandler(event) {}
+/**
+ * EVENT HANDLERS
+ */
+// UPON PAGE LOAD
+var home, map, events, settings; // booleans for page status
+
+async function pageLoadHandler(event) {
+  // Set home to true/visible, all other pages should be hidden
+  home = true;
+  map = false;
+  events = false;
+  settings = false;
+  // set all other pages to hide
+}
 
 document.addEventListener("DOMContentLoaded", pageLoadHandler);
+
+// SHOW PAGES BASED ON NAV
+// event listener for each icon
+
+/**
+ * SMALL/HELPER FUNCTIONS
+ */
+
+// SHOW IF PAGE = TRUE
+function showPage() {
+  if (home == true) {
+    // show lang page, click english to get to home
+  }
+  // else if etc
+}
