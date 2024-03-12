8ea19ae8697bf0a2fb2d38c403233ea469f067e3
lghavami <leylighavami@gmail.com>
2024-03-12 14:58:28 +0100

    fixed issue with gallery header and show homepage immediately after language is selected

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-let currentLanguage = '';
+let currentLanguage = "";
 
 // Set the default page to 'home'
-changePage('home');
+changePage("home");
 
 function changeLanguage(language) {
-    currentLanguage = language;
-    // You can add logic to update content based on the selected language
-    document.getElementById('language-selection').style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-    document.getElementById('header').style.display = 'none'; // Hide header on language selection
-    document.getElementById('gallery-header').style.display = 'none'; // Hide gallery header on language selection
+  currentLanguage = language;
+  // You can add logic to update content based on the selected language
+  document.getElementById("language-selection").style.display = "none";
+  document.getElementById("content").style.display = "block";
+  document.getElementById("header").style.display = "none"; // Hide header on language selection
+  document.getElementById("gallery-header").style.display = "none"; // Hide gallery header on language selection
+
+  // route to homepage
+  changePage(home);
 }
 
 function changePage(page) {
-    // Hide all pages
-    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
-    // Show the selected page
-    document.getElementById(page).classList.add('active');
+  // Hide all pages
+  document
+    .querySelectorAll(".page")
+    .forEach((p) => p.classList.remove("active"));
+  // Show the selected page
+  document.getElementById(page).classList.add("active");
 
-    // Show or hide the gallery header based on the page
-    document.getElementById('gallery-header').style.display = (page !== 'home') ? 'block' : 'none';
+  // Show or hide the gallery header based on the page
+  document.getElementById("gallery-header").style.display = "block";
 }
